9127e5d2ca08adf45d91142a0a36299d86f7aaa9
siam666-shell <abdullahsiam009@gmail.com>
2022-05-17 13:32:13 +0600

    typed js animation done

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Typed from 'react-typed';
 
 const Home = () => {
   const settings = {
@@ -9,6 +10,46 @@ const Home = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
+  const typedString = [
+    'Complete Graphics Designer',
+    'Complete Front End Developer',
+    'Complete Back End Developer',
+    'Complete javascript Developer',
+  ];
+
+  const options = {
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 50,
+    // time before typing starts
+    startDelay: 500,
+    // backspacing speed
+    backSpeed: 50,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 100,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: '|',
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function () {},
+    // starting callback function before each string
+    preStringTyped: function () {},
+    //callback for every typed string
+    onStringTyped: function () {},
+    // callback for reset
+    resetCallback: function () {},
+  };
   return (
     <section id="banner-part" name="banner-part">
       <div className="container">
@@ -17,7 +58,10 @@ const Home = () => {
             <div className="col-lg-10 m-auto">
               <div className="banner-text text-center">
                 <h2>
-                  We Are <span className="typed"></span>
+                  We Are{' '}
+                  <span className="typed">
+                    <Typed strings={typedString} {...options} />
+                  </span>
                 </h2>
                 <p>
                   This is Photoshop's version of Lorem Ipsum. Proin gravida nibh
